e13663babc2480225f05e6292bf7b40254f454f3
Aniket <patilaniket1307@gmail.com>
2022-04-29 13:41:58 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(
+  <div>
+    <div> <App /> </div>
+  </div>,
+  document.getElementById('root')
+);
